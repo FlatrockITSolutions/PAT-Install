b06935ed2e3669b9b6d8dfb1fba45ef5e7f1d35d
Flatrock Information Technology Solutions LLC <112456263+FlatrockITSolutions@users.noreply.github.com>
2025-02-11 09:30:09 -0800

    Add files via upload

---
+++ pat_install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+wget raw.github.com/tritakanguyen/PAT/main/PickAssistant.py ~
+
+if ls /usr/local/bin | grep -q PickAssistant.py; then
+    if diff -q ~/PickAssistant.py /usr/local/bin/PickAssistant.py; then
+	echo "alias picklist='python3 /usr/local/bin/PickAssistant.py'" >> ~/.zshrc
+	source ~/.zshrc > /dev/null
+	echo "PickAssistant is up to date."
+ 	sudo rm ~/PickAssistant.py*
+    else
+        echo "Pick Assistant is out of date; updating"
+        sudo mv ~/PickAssistant.py /usr/local/bin/PickAssistant.py
+        if cat ~/.zshrc | grep -q 'picklist'; then
+            echo "Pick Assistant Tool is ready to use."
+        else
+            echo "alias picklist='python3 /usr/local/bin/PickAssistant.py'" >> ~/.zshrc
+            source ~/.zshrc > /dev/null
+            echo "Pick Assistant Tool is ready to use"
+        fi
+    fi
+else
+    sudo mv ~/PickAssistant.py /usr/local/bin/PickAssistant.py
+    if cat ~/.zshrc | grep -q 'picklist'; then
+        echo "Pick Assistant Tool is ready to use."
+    else
+        echo "alias picklist='python3 /usr/local/bin/PickAssistant.py'" >> ~/.zshrc
+        source ~/.zshrc > /dev/null
+        echo "Pick Assistant Tool is ready to use"
+    fi
+fi
